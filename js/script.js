3f421314b2784bf21c1b4e85be920868f64d7304
younghyun <naem77716@gmail.co.kr>
2023-06-29 15:02:55 +0900

    해더 페이지 작성 및 스타일 업데이트

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
-$(document).ready(function () {
-	common_load(); // 문서로드
-	Swiper_event(); // 슬라이드 이벤트
-	selectBox(); // 셀렉트 박스 이벤트
-	// aside_event () // 사이드바 이벤트
-	tab_event(); // 탭 메뉴 이벤트
-	btnClick_event(); // 클릭 이벤트
-});
+// $(document).ready(function () {
+// 	common_load(); // 문서로드
+// 	Swiper_event(); // 슬라이드 이벤트
+// 	selectBox(); // 셀렉트 박스 이벤트
+// 	// aside_event () // 사이드바 이벤트
+// 	tab_event(); // 탭 메뉴 이벤트
+// 	btnClick_event(); // 클릭 이벤트
+// });
 
-$('.footer').load('footer.html', function () {});
-$('.header').load('header.html', function () {});
+$(document).ready(function () {
+	$('.footer').load('footer.html', function () {
+		console.log('들어옴');
+	});
+	$('.header').load('header.html', function () {});
+});
